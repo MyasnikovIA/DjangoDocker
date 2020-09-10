bcd942ca1aa97dd74135b4f02d0aec9b8f867b60
MyasnikovIA <Miacom@mail.ru>
2020-09-10 23:11:23 +0700

    init

--- build.bat
+++ build.bat
@@ -1,12 +1,10 @@
-
-rem docker build -t med_mis . 
-docker-compose build  
-docker-compose run ddda sh -c “django-admin.py startproject app .”
+docker-compose build
+docker rmi python:3.7-alpine  
+rem docker-compose run ddda sh -c “django-admin.py startproject app .”
+rem docker-compose exec ddda python manage.py migrate
 docker-compose up -d
 timeout 10
 start "" "http://127.0.0.1:8888/" 
-
-
 
 
 rem docker-compose run ddda sh -c “django-admin.py startproject app .”
